bfabee34b5ecd175f1b0138bcab1a3dd7e25893c
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-23 19:03:06 +0530

    feat: added context provider to tree

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ProductsContextProvider } from "./Contexts/productsContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ProductsContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ProductsContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
