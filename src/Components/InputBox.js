c5944a509394e96a35307db3073d30267a0dd3e3
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-30 19:02:35 +0530

    feat: resusable input component

---
+++ src/Components/InputBox.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const InputBox = ({
+  label,
+  id,
+  placeholder,
+  value,
+  callback,
+  type = "text",
+}) => {
+  return (
+    <>
+      <label htmlFor={id} className="font-semibold tracking-wide">
+        {label}
+      </label>
+      <input
+        type={type}
+        placeholder={placeholder}
+        id={id}
+        value={value}
+        onChange={callback}
+        className="border w-full p-1 rounded-sm shadow-sm tracking-wide my-1"
+      />
+    </>
+  );
+};
+
+export default InputBox;
