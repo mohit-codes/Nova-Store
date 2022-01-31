1d97fe4b5449d0d1e27e984e2abddb64a266d4ca
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-31 19:27:10 +0530

    feat: exports

--- src/Components/ProductsPageComponents/index.js
+++ src/Components/ProductsPageComponents/index.js
@@ -1,3 +1,5 @@
 export { Product } from "./ProductCard/Product";
 export { SortBy } from "./SortByDropDown/SortBy";
 export { CheckBox } from "./CheckBox";
+export { ToggleWishlist } from "./ProductCard/ToggleWishlist";
+export { AddToCart } from "./ProductCard/AddToCart";
