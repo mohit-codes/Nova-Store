2e3d2827493ce4a06ebaba0af515fbe289a18ef3
mohit kushwaha <kushwahamohit000@gmail.com>
2022-01-31 19:29:54 +0530

    feat: added user data context

---
+++ src/Contexts/userDataContext.js
@@ -0,0 +1,21 @@
+import { createContext, useContext, useReducer } from "react";
+import { userReducer } from "../reducers/userReducer";
+
+export const userDataContext = createContext();
+
+export const UserDataProvider = ({ children }) => {
+  const initialState = {
+    cart: [],
+    wishlist: [],
+  };
+
+  const [userData, userDispatch] = useReducer(userReducer, initialState);
+
+  return (
+    <userDataContext.Provider value={{ userData, userDispatch }}>
+      {children}
+    </userDataContext.Provider>
+  );
+};
+
+export const useUserData = () => useContext(userDataContext);
