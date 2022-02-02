eefa2d2f4902a24894760be7ab65f7cf8a91682c
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-02 19:30:04 +0530

    feat: cart page listing cart items

---
+++ src/Pages/Cart.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import { useUserData } from "../Contexts/userDataContext";
+import { useAuth } from "../hooks/useAuth";
+import { Loading } from "../Components/Loading";
+import axios from "axios";
+import { BASE_URL } from "../utils/utility";
+import { CartItem, CheckoutBox } from "../Components/CartPageComponents";
+
+export const Cart = () => {
+  const { userProfile } = useAuth();
+  const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { userData } = useUserData();
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const { data } = await axios.get(
+        `${BASE_URL}/carts/fetch-cart/${userProfile._id}`
+      );
+      setCart(data.items || []);
+      setLoading(false);
+    })();
+  }, [userData.cart]);
+
+  const isCartEmpty = cart.length < 1;
+  return (
+    <div className="pt-14 max-w-7xl ml-auto mr-auto h-full">
+      <header className="p-8 text-xl font-semibold">
+        <h2>Your cart</h2>
+      </header>
+      {loading && !isCartEmpty ? (
+        <div className="flex justify-center items-center h-32">
+          <Loading withContainer={false} />
+        </div>
+      ) : (
+        <div className="flex justify-center h-full">
+          <div className="m-2">
+            {cart.map((item) => (
+              <CartItem key={item.product._id} item={item} />
+            ))}
+          </div>
+          <CheckoutBox cart={cart} />
+        </div>
+      )}
+      {!loading && isCartEmpty && (
+        <div className="flex justify-center items-center h-48 text-slate-500 font-semibold text-lg">
+          No products in cart!
+        </div>
+      )}
+    </div>
+  );
+};
