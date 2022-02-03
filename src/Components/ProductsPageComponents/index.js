a487281b60403492ad88c67c65538c97ae21e881
mohit kushwaha <kushwahamohit000@gmail.com>
2022-02-03 16:45:53 +0530

    chore: component exports

--- src/Components/ProductsPageComponents/index.js
+++ src/Components/ProductsPageComponents/index.js
@@ -1,5 +1,9 @@
 export { Product } from "./ProductCard/Product";
 export { SortBy } from "./SortByDropDown/SortBy";
 export { CheckBox } from "./CheckBox";
-export { ToggleWishlist } from "./ProductCard/ToggleWishlist";
+export {
+  ToggleWishlistIcon,
+  ToggleWishlistButton,
+} from "./ProductCard/ToggleWishlist";
 export { AddToCart } from "./ProductCard/AddToCart";
+export { Tag } from "./Tag";
